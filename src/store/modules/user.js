58b7489195b3e758476964a540fb10b5e9be2d64
kelly227800 <2529303733@qq.com>
2022-08-02 10:45:49 +0800

    完成登录流程

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,21 @@
+import { login } from "@/api/user";
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
-}
-
+  state: {
+    token: "",
+  },
+  mutations: {
+    setToken(state, payload) {
+      state.token = payload;
+    },
+  },
+  actions: {
+    async getToken(context,payload) {
+      // 发送请求得来的
+      const res = await login(payload)
+      console.log(res);
+      context.commit('setToken', res);
+    }
+  },
+};
